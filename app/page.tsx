b74f51dec36eaf42e00df6c9e4997509d9166b4d
Kamil Starmach <kstarmach@outlook.com>
2024-03-17 18:10:33 +0100

    first update

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,45 @@
-import Image from "next/image";
+import { SunIcon, BoltIcon, BeakerIcon, CakeIcon, Battery50Icon } from "@heroicons/react/16/solid";
 
-export default function Home() {
+interface TaskProps {
+  icon: JSX.Element;
+  number: string;
+  title: string;
+  description: string;
+  key: string; // Add key prop to TaskProps
+}
+
+const TaskItem = ({ icon, number, title, description, key }: TaskProps) => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex flex-col lg:flex-row justify-between" key={key}>
+      <div className="flex items-center gap-6 mb-4 lg:mb-0">
+        <div className="bg-stone-800 rounded-full p-4">{icon}</div>
+        <div className="flex flex-col">
+          <div className="flex items-center">
+            <span className="text-5xl w-4 mr-8">{number}</span>
+            <h2 className="text-3xl uppercase">{title}</h2>
+          </div>
+          <p className="text-lg max-w-[800px]">{description}</p>
         </div>
       </div>
+      <input type="checkbox" className="cursor-pointer w-10 border-4 border-stone-800 rounded-sm bg-inherit align-self-end" />
+    </div>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-6 lg:p-24">
+      <div className="text-5xl font-medium text-center">
+        <h1>DO YOUR</h1>
+        <h1 className="text-white bg-stone-800 text-7xl p-4 m-3">DAILIES.</h1>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="flex flex-col gap-4 mt-8">
+        <TaskItem key="1" icon={<SunIcon className="w-10 text-white" />} number="1." title="Sunlight" description="At least 20 minutes direct sunlight." />
+        <TaskItem key="2" icon={<BoltIcon className="w-10 text-white" />} number="2." title="Movement" description="45+ minutes movement of choice (basketball, walk, swim, run, weight train, etc.)" />
+        <TaskItem key="3" icon={<BeakerIcon className="w-10 text-white" />} number="3." title="Water" description="Don't forget to stay hydrated! Plain, good ol' H2O" />
+        <TaskItem key="4" icon={<CakeIcon className="w-10 text-white" />} number="4." title="Eat" description="Consume protein and a variety of fruits & veggies with each meal (to fuel your body with the nutrients it needs to keep you strong & healthy)." />
+        <TaskItem key="5" icon={<Battery50Icon className="w-10 text-white" />} number="5." title="Sleep" description="8+ hours per night!" />
       </div>
     </main>
   );
-}
+};
